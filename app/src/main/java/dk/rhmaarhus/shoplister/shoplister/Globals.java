ba8ca8a272f24881600f00fad0e0c69aaeaf6b7f
Monika Ruszczyk <ruszczyk.monika@gmail.com>
2017-11-30 15:53:54 +0100

    flow added main>details>share

--- app/src/main/java/dk/rhmaarhus/shoplister/shoplister/Globals.java
+++ app/src/main/java/dk/rhmaarhus/shoplister/shoplister/Globals.java
@@ -6,4 +6,9 @@ package dk.rhmaarhus.shoplister.shoplister;
 
 public class Globals {
     public static final String TAG = "shopListerLog";
+    public static final String LIST_NAME = "listName";
+    public static final int LIST_DETAILS_REQ_CODE = 1;
+    public static final int SHARE_SCREEN_REQ_CODE = 2;
+
+
 }
