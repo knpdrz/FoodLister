a6eaef59dc7f92766c42e81815a3361a3bf9e38e
rjkey <richardjoakey@gmail.com>
2017-11-30 18:19:01 +0100

    Some code added

--- app/src/main/java/dk/rhmaarhus/shoplister/shoplister/AddShoppingItemActivity.java
+++ app/src/main/java/dk/rhmaarhus/shoplister/shoplister/AddShoppingItemActivity.java
@@ -1,13 +1,41 @@
 package dk.rhmaarhus.shoplister.shoplister;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class AddShoppingItemActivity extends AppCompatActivity {
 
+    private Button searchBtn;
+    private ListView foodListView;
+    private EditText searchField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_shopping_item);
+
+        searchBtn = findViewById(R.id.searchItemBtn);
+        foodListView = findViewById(R.id.searchFoodList);
+        searchField = findViewById(R.id.findFoodText);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SearchForFood();
+            }
+        });
+
+    }
+
+    private void SearchForFood() {
+
     }
 }
