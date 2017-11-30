46ec20f8bf4ff5c69eb23eeff6cecd3dc3a8dfa9
rjkey <richardjoakey@gmail.com>
2017-11-30 17:10:12 +0100

    added internet permission in manifest

--- app/src/main/java/dk/rhmaarhus/shoplister/shoplister/ListsActivity.java
+++ app/src/main/java/dk/rhmaarhus/shoplister/shoplister/ListsActivity.java
@@ -1,15 +1,14 @@
 package dk.rhmaarhus.shoplister.shoplister;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
